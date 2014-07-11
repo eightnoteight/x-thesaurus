6a866747101745312d4c34ed19ae907c2481b0cd
eightnoteight <mr.eightnoteight@gmail.com>
2014-07-11 22:56:41 +0530

    http-get cpp wraper coded

---
+++ lib/http-get/http-get.cpp
@@ -0,0 +1,43 @@
+#include "http-get.c"
+class GET
+{
+private:
+	std::string randstr(int len);
+	std::string file(std::string url);//returns filename
+public:
+	int ok;
+	long long int status;
+	std::string msg;
+	std::string filename;
+	GET(std::string url);
+
+	/* data */
+};
+GET::GET(std::string url="http://www.google.com")
+{
+	http_get_response_t *res=http_get(url.c_str());
+	ok = res->ok;
+	status = res->status;
+	std::string tmp (res->data,res->size);
+	msg = tmp;
+	filename = file(url);
+	http_get_free(res);	
+}
+std::string GET::randstr(int len)
+{
+	const char alphanum[] = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	std::string str;
+	srand(time(NULL));
+	for (int i = 0; i < len; ++i)
+	{
+		str.push_back(alphanum[rand() % strlen(alphanum)]);
+	}
+	return str;
+}
+std::string GET::file(std::string url)
+{
+	std::string filname = randstr(15);
+	if (http_get_file(url.c_str(), ("/tmp/" + filname).c_str()) == 0)
+		return "/tmp/" + filname;
+	return "null";
+}
