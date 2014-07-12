a90b4e77799b0b65db19bdcb0b5823ea21f9b228
eightnoteight <mr.eightnoteight@gmail.com>
2014-07-12 15:56:33 +0530

    TO-DO : complete parsing part and have fun

--- src/x-thesaurus.cpp
+++ src/x-thesaurus.cpp
@@ -11,25 +11,47 @@ int main(int argc, char const *argv[])
 	Geopt opt (argc, argv);
 	for (int i = 1; i < words; ++i)
 	{
-		GET get("http://words.bighugelabs.com/api/2/" + api_key + "/" + opt.get_next() + "/json");
-		std::cout << get.ok << std::endl;
-		std::cout << get.status << std::endl;
-		std::cout << get.msg << std::endl;
-		Json::Value root;
-		Json::Reader reader;
-		std::cout << get.filename << std::endl;
-		std::ifstream file;
-		file.open((get.filename).c_str());
-		std::istream& inp = file;
-		bool parsingSuccessful = reader.parse(inp,root);
-		file.close();
-		if ( !parsingSuccessful )
+		std::string which_word = opt.get_next();
+		GET get("http://words.bighugelabs.com/api/2/" + api_key + "/" + which_word + "/json");
+		if (!get.ok)
 		{
-			std::cout << "Failed to parse configuration\n"
-			<< reader.getFormattedErrorMessages();
-			continue;
+			switch(get.status)
+			{
+				case 404:
+					std::cout << \
+					"[x-thesaurus] Sorry couldn't find the word: "\
+					+ which_word << std::endl;
+					break;
+				case 500:
+					std::cout << \
+					"[x-thesaurus] Usage limit exceeded or Invalid Key"\
+					<< std::endl;
+					break;
+				case 0:
+					std::cout << std::string("[x-thesaurus] Connection Time Out!\n") +\
+					std::string("[x-thesaurus] Check your Internet Connection!")\
+					<< std::endl;
+					break;
+			}
 		}
-		std::cout << root << std::endl;
+		else
+		{
+			Json::Value root ;
+			Json::Reader reader;
+			std::cout << get.filename << std::endl;
+			std::ifstream file;
+			file.open((get.filename).c_str());
+			std::istream& inp = file;
+			bool parsingSuccessful = reader.parse(inp, root);
+			file.close();
+			if ( !parsingSuccessful )
+			{
+				std::cout << "Failed to parse configuration\n"
+				<< reader.getFormattedErrorMessages();
+				continue;
+			}
+			std::cout << root << std::endl;
+		}
 
 	}
 	return 0;
