1252b25cba1b2ec0ca711e12276df4b17b05ff80
eightnoteight <mr.eightnoteight@gmail.com>
2014-07-19 15:31:13 +0530

    constructers for colors structs instead of initializers

--- src/colors.h
+++ src/colors.h
@@ -8,6 +8,7 @@ struct fore {
 	std::string magenta;
 	std::string cyan;
 	std::string white;
+    fore();
 } fg;
 struct back {
 	std::string black;
@@ -18,10 +19,43 @@ struct back {
 	std::string magenta;
 	std::string cyan;
 	std::string white;
+    back();
 } bg;
 struct style {
 	std::string bold;
 	std::string blink;
 	std::string reset;
 	std::string rvid;
+    style();
 } st;
+
+fore::fore() {
+
+	black	= "\033[30m";
+	red 	= "\033[31m";
+	green	= "\033[32m";
+	yellow	= "\033[33m";
+	blue 	= "\033[34m";
+	magenta	= "\033[35m";
+	cyan	= "\033[36m";
+	white	= "\033[37m";
+	
+}
+back::back() {
+	black	= "\033[40m";
+	red 	= "\033[41m";
+	green 	= "\033[42m";
+	yellow	= "\033[43m";
+	blue 	= "\033[44m";
+	magenta	= "\033[45m";
+	cyan	= "\033[46m";
+	white	= "\033[47m";
+	
+}
+style::style() {
+
+	bold	= "\033[1m";
+	blink	= "\033[5m";
+	reset	= "\033[0m";
+	rvid	= "\033[7m";
+}
--- src/init__x-thesaurus.h
+++ src/init__x-thesaurus.h
@@ -23,26 +23,5 @@ inline bool init(std::list<std::string>& partsofspeech,
 	reltypmap["usr"] = "User Suggestions";
 	reltypmap["rel"] = "Related Terms";
 	reltypmap["sim"] = "Similar Types";
-
-	fg.black	= "\033[30m";
-	fg.red 		= "\033[31m";
-	fg.green	= "\033[32m";
-	fg.yellow	= "\033[33m";
-	fg.blue 	= "\033[34m";
-	fg.magenta	= "\033[35m";
-	fg.cyan		= "\033[36m";
-	fg.white	= "\033[37m";
-	bg.black	= "\033[40m";
-	bg.red 		= "\033[41m";
-	bg.green 	= "\033[42m";
-	bg.yellow	= "\033[43m";
-	bg.blue 	= "\033[44m";
-	bg.magenta	= "\033[45m";
-	bg.cyan		= "\033[46m";
-	bg.white	= "\033[47m";
-	st.bold		= "\033[1m";
-	st.blink	= "\033[5m";
-	st.reset	= "\033[0m";
-	st.rvid		= "\033[7m";
 	return 1;
 }
